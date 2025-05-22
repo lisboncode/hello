469d5a2f3c4f62b268aa004145cf76e3e2d6276d
lisboncode <lisboncode@gmail.com>
2025-05-22 15:46:01 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+def say_hi():
+    print('Hi, my name is Sérgio Rodrigues and I am a teacher.')
+
+__version__ = '1.0'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='SHR',
+      author_email='sergio@rodrigues.com',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
